488818c43845b137cbb6185e6bd453f6a8211fa1
Marko Leppänen <anterolep@gmail.com>
2022-02-07 18:59:39 +0200

    rename

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,4 +1,4 @@
-export const GAME_TITLE = 'FFXIVDLE'
+export const GAME_TITLE = 'FFXIRVDLE'
 
 export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
